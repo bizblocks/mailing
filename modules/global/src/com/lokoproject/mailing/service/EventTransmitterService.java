abc28fa52c88135de3b469d550b1b3a5bb1e32e3
neantonio <a.o.lomako@gmail.com>
2019-04-26 10:44:04 +0300

    eventTransmitterService was not commited

---
+++ modules/global/src/com/lokoproject/mailing/service/EventTransmitterService.java
@@ -0,0 +1,10 @@
+package com.lokoproject.mailing.service;
+
+/**
+ * Created by Anton on 25.04.2019.
+ */
+public interface EventTransmitterService {
+    String NAME = "mailing_EventTransmitterService";
+
+    void setUrlOfWebModule(String url);
+}
